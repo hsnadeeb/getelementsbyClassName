55344bb10d42919ef957aeb91cb66878de2a7e96
Hasan Adeeb <hsnadeeb007@gmail.com>
2023-03-30 21:44:27 +0530

    visibility comit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var items=document.getElementsByClassName('list-group-item');
 
 items[2].style.backgroundColor='green';
-
+items[1].style.backgroundColor='green';
+items[2].style.visibility = "hidden";
 
 for(var i=0;i<items.length;i++)
 {
@@ -11,6 +12,7 @@ for(var i=0;i<items.length;i++)
 
 var newitem=document.getElementsByClassName('new-list-item');
 newitem[0].style.backgroundColor='red';
+
 
 
 var tagnameitem=document.getElementsByTagName('li');
